6eaa4a41c8793a00fe88fb88e76e34728abd810c
l.vannes <l.vannes@arvoo.com>
2026-02-05 16:33:08 +0100

    sdfa

--- assets/howler-DrXzFssz.js
+++ assets/howler-DrXzFssz.js
@@ -1,4 +1,4 @@
-import{b as e}from"./index-yGha8cpZ.js";var t=e((exports=>{
+import{b as e}from"./index-CIp0kPKw.js";var t=e((exports=>{
 /*!
 *  Spatial Plugin - Adds support for stereo and 3D audio where Web Audio is supported.
 *  

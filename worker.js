c8aa521de7254c0ca785ef906574d90c60ceaf37
l.vannes <l.vannes@arvoo.com>
2026-03-03 12:47:45 +0100

    pojkfsd

--- worker.js
+++
@@ -1,8 +0,0 @@
-var interval = self.setInterval(() => {
-  if (self.registration) {
-    self.registration.showNotification('Hello from main script').catch((e) => {
-      console.error(e)
-      clearInterval(interval)
-    })
-  }
-}, 10000)
